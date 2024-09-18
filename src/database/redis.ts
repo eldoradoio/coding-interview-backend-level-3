c56b9d1efcd50b8bff14b4c53324a40b9b007628
aragornz325 <rodrigo.m.quintero@outlook.com>
2024-09-18 11:46:08 +0000

    correccion en la documentacion

--- src/database/redis.ts
+++ src/database/redis.ts
@@ -1,28 +1,31 @@
 import Redis from 'ioredis';
 import { config } from '../config';
 
-
 console.log(`Trying to connect to Redis at ${config.REDIS_HOST}:${config.REDIS_PORT}`);
 
-
-
 /**
- * Configuración de Redis - variables de entorno - 
- * host: IP o nombre del host de Redis (Va host)
- * port: Puerto de Redis
- * connectTimeout: Tiempo de espera para conectar a Redis
+ * Configuración de Redis
+ * 
+ * Parámetros:
+ * - host: Dirección IP o nombre de host donde se está ejecutando Redis.
+ * - port: Puerto donde Redis está escuchando (normalmente 6379).
+ * - connectTimeout: Tiempo máximo de espera en milisegundos para establecer la conexión antes de fallar. 
+ * - retryStrategy: Estrategia de reintento en caso de fallo de conexión. Aumenta el tiempo entre reintentos basado en el número de intentos. 
+ *   En este caso, comenzará con 100ms y se incrementará hasta un máximo de 2000ms por intento.
+ * - maxRetriesPerRequest: Número máximo de reintentos por solicitud antes de fallar. 'null' permite un número ilimitado de reintentos.
  */
 const redis = new Redis({
-  host: 'redis',  // IP o nombre del host de Redis (Va host)
-  port: config.REDIS_PORT,  
+  host: 'redis',  
+  port: config.REDIS_PORT, 
   connectTimeout: config.REDIS_TIMEOUT || 10000,  
   retryStrategy(times) {
+   
     return Math.min(times * 100, 2000);  
   },
   maxRetriesPerRequest: null,  
 });
 
-//monitor de eventos
+// Monitor de eventos de Redis para mejor diagnóstico y control
 redis.on('connect', () => {
   console.log('Connected to Redis');
 });
