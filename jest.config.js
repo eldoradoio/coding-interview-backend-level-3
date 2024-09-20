6e7fef9caff0f68221a9701c7398ea2d2a7b60db
Facundo Bertoli <fabertoli@teco.com.ar>
2024-09-20 16:06:08 -0300

    archivos de configuracion necesarios para ejecutar los test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
+    preset: 'ts-jest',
     transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
+      '^.+\\.(t|j)sx?$': ['@swc/jest'],
     },
-   }
-   
+    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    testEnvironment: 'node',
+    testMatch: ['**/?(*.)+(spec|test).ts?(x)'], 
+    setupFilesAfterEnv: ['./jest.setup.js']
+  };
+  
