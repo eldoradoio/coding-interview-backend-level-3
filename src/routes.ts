6e2a8d3b88fca244c2fd7cc2d13b0c246d984f4e
Facundo Bertoli <fabertoli@teco.com.ar>
2024-09-20 16:07:42 -0300

    se agregan rutas necesarias para cumplir con los test

--- src/routes.ts
+++ src/routes.ts
@@ -37,30 +37,64 @@ export const defineRoutes = (server: Server) => {
       return item;
     },
   });
+
   server.route({
     method: "POST",
     path: "/items",
     handler: async (request, h) => {
+      const { name, price } = request.payload as {
+        name: string;
+        price: number;
+      };
+      if (price === undefined) {
+        return h
+          .response({
+            errors: [{ field: "price", message: 'Field "price" is required' }],
+          })
+          .code(400);
+      }
+      if (price < 0) {
+        return h
+          .response({
+            errors: [{ field: "price", message: 'Field "price" cannot be negative' }],
+          })
+          .code(400);
+      }
       const itemRepository = AppDataSource.getRepository(Item);
       const newItem = itemRepository.create(request.payload as Item);
       const result = await itemRepository.save(newItem);
       return h.response(result).code(201);
     },
   });
+
   server.route({
     method: "PUT",
     path: "/items/{id}",
     handler: async (request, h) => {
+      const { name, price } = request.payload as {
+        name: string;
+        price: number;
+      };
+      if (price < 0) {
+        return h
+          .response({
+            errors: [
+              { field: "price", message: 'Field "price" cannot be negative' },
+            ],
+          })
+          .code(400);
+      }
       const itemRepository = AppDataSource.getRepository(Item);
-      const item = await itemRepository.findOneBy({
-        id: Number(request.params.id),
-      });
+      const itemId = parseInt(request.params.id, 10);
+      const item = await itemRepository.findOneBy({ id: itemId });
       if (!item) {
         return h.response({ error: "Item not found" }).code(404);
       }
-      const updatedItem = Object.assign(item, request.payload);
-      const result = await itemRepository.save(updatedItem);
-      return result;
+
+      item.name = name;
+      item.price = price;
+      const updatedItem = await itemRepository.save(item);
+      return h.response(updatedItem).code(200);
     },
   });
   server.route({
