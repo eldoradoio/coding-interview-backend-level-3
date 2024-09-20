b5fcb60af8c5e45921e85bd698cecc8c6f85bf39
Facundo Bertoli <fabertoli@teco.com.ar>
2024-09-20 16:09:05 -0300

    archivos de configuracion de la db y en general

---
+++ src/config/config.ts
@@ -0,0 +1,36 @@
+import * as dotenv from 'dotenv';
+import * as joi from 'joi';
+
+dotenv.config();
+
+const envSchema = joi.object({
+    NODE_ENV: joi.string().valid('development', 'test', 'production').default('development'),
+    DB_HOST: joi.string().required(),
+    DB_PORT: joi.number().required(),
+    DB_USER: joi.string().required(),
+    DB_PASSWORD: joi.string().required(),
+    DB_NAME: joi.string().required(),
+    TEST_DB_HOST: joi.string().optional(),
+    TEST_DB_PORT: joi.number().optional(),
+    TEST_DB_USER: joi.string().optional(),
+    TEST_DB_PASSWORD: joi.string().optional(),
+    TEST_DB_NAME: joi.string().optional().default(':memory:')
+}).unknown(); 
+
+const { value: envVars, error } = envSchema.validate(process.env);
+
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+export const config = {
+    env: envVars.NODE_ENV,
+    db: {
+        type: envVars.NODE_ENV === 'test' && envVars.TEST_DB_NAME === ':memory:' ? 'sqlite' : 'postgres',
+        host: envVars.NODE_ENV === 'test' && envVars.TEST_DB_NAME === ':memory:' ? undefined : envVars.TEST_DB_HOST || envVars.DB_HOST,
+        port: envVars.NODE_ENV === 'test' && envVars.TEST_DB_NAME === ':memory:' ? undefined : envVars.TEST_DB_PORT || envVars.DB_PORT,
+        username: envVars.NODE_ENV === 'test' && envVars.TEST_DB_NAME === ':memory:' ? undefined : envVars.TEST_DB_USER || envVars.DB_USER,
+        password: envVars.NODE_ENV === 'test' && envVars.TEST_DB_NAME === ':memory:' ? undefined : envVars.TEST_DB_PASSWORD || envVars.DB_PASSWORD,
+        database: envVars.NODE_ENV === 'test' ? envVars.TEST_DB_NAME : envVars.DB_NAME,
+    }
+};
--- src/config/database/ormconfig.ts
+++ src/config/database/ormconfig.ts
@@ -1,16 +1,17 @@
 import { DataSource } from 'typeorm';
 import { Item } from '../../entities/item.entities';
+import { config } from '../config';
 
 export const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '041086',
-    database: 'doradodb',
-    synchronize: false, 
-    logging: true,
+    type: process.env.NODE_ENV === 'test' ? 'sqlite' : 'postgres',
+    host: process.env.NODE_ENV === 'test' ? undefined : config.db.host,
+    port: process.env.NODE_ENV === 'test' ? undefined : config.db.port,
+    username: process.env.NODE_ENV === 'test' ? undefined : config.db.username,
+    password: process.env.NODE_ENV === 'test' ? undefined : config.db.password,
+    database: process.env.NODE_ENV === 'test' ? ':memory:' : config.db.database,
+    synchronize: true,
+    logging: false,
     entities: [Item],
-    migrations: [__dirname + '/migrations/*.ts'],
+    migrations: [__dirname + '/../migrations/*.ts'],
     subscribers: [],
 });
