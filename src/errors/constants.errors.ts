3cf60ded3825f0a282d0c233cafb7e613a415ddb
Facundo Bertoli <fabertoli@teco.com.ar>
2024-09-21 18:42:36 -0300

    Se agrega el valor que restaba FAILED_TO_FETCH_ITEM

--- src/errors/constants.errors.ts
+++ src/errors/constants.errors.ts
@@ -4,4 +4,5 @@ export const ErrorBussines = {
     ITEM_NOT_FOUND: 'Item not found',
     INTERNAL_ERROR: 'Internal Server Error',
     FAILED_TO_CREATE_ITEM: 'Failed to create item',
+    FAILED_TO_FETCH_ITEM: 'Failed to fetch item'
 }
