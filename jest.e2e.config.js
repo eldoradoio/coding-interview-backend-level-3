c1daf40a5d2b3bd9242125424e3cae0e48309924
Bryan Key <bryan.hernandez@lernit.com.mx>
2024-12-09 15:48:46 -0400

    [ADD] all test pass

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     moduleFileExtensions: ['js', 'json', 'ts'],
-    // testRegex: '.*\\.test\\.ts$', // Solo archivos .test.ts
-    testRegex: '.index_nestjs\\.test\\.ts$', // Solo archivos .test.ts
+    // testRegex: '.*\\.test\\.ts$',
+    testRegex: '.index_nestjs\\.test\\.ts$', 
     transform: {
       '^.+\\.(t|j)s$': 'ts-jest',
     },
     rootDir: '.',
-    testEnvironment: 'node', // Cambiar de '@hapi/hapi' a 'node'
+    testEnvironment: 'node',
   };
   
